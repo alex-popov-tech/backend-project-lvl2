dec5e9336aad9d3f75be28da586f2a0f6f6db7bc
Alexander Popov <alex.popov.tech@gmail.com>
2020-05-01 17:51:59 +0300

    refacor diff generation

--- __tests__/diffGenerator.test.js
+++ __tests__/diffGenerator.test.js
@@ -1,46 +1,96 @@
 import genDiff from '../src/diffGenerator.js';
 
-const before = {
-  a: 1, b: true, c: 'three', e: {},
-  gr: {
-    a: 1, b: true, c: 'three', e: {},
-    dgr: {
-      a: 1, b: true, c: 'three', e: {},
-    },
-  },
-};
 
-const after = {
-  b: false, c: 'three', d: 'new', f: {},
-  gr: {
-    b: false, c: 'three', d: 'new', f: {},
-    dgr: {
-      b: false, c: 'three', d: 'new', f: {},
+test('should return correct diff object when comparing flat objects', () => {
+  const before = {
+    a: 1,
+    b: true,
+    c: 'three',
+    e: {},
+  };
+  const after = {
+    b: false,
+    c: 'three',
+    d: 'new',
+    f: {},
+  };
+  expect(genDiff(before, after)).toEqual([
+    {
+      name: 'a',
+      from: 1,
+      to: undefined,
+    }, {
+      name: 'b',
+      from: true,
+      to: false,
+    }, {
+      name: 'c',
+      from: 'three',
+      to: 'three',
+    }, {
+      name: 'd',
+      from: undefined,
+      to: 'new',
+    }, {
+      name: 'e',
+      from: {},
+      to: undefined,
+    }, {
+      name: 'f',
+      from: undefined,
+      to: {},
     },
-  },
-};
+  ]);
+});
+
 
 test('should return correct diff object when comparing deep objects', () => {
-  expect(genDiff(before, after)).toEqual({
-    added: [{ key: 'd', value: 'new' }, { key: 'f', value: {} }],
-    removed: [{ key: 'a', value: 1 }, { key: 'e', value: {} }],
-    untouched: [{ key: 'c', value: 'three' }],
-    updated: [{ key: 'b', from: true, to: false }, {
-      key: 'gr',
-      value: {
-        added: [{ key: 'd', value: 'new' }, { key: 'f', value: {} }],
-        removed: [{ key: 'a', value: 1 }, { key: 'e', value: {} }],
-        untouched: [{ key: 'c', value: 'three' }],
-        updated: [{ key: 'b', from: true, to: false }, {
-          key: 'dgr',
-          value: {
-            added: [{ key: 'd', value: 'new' }, { key: 'f', value: {} }],
-            removed: [{ key: 'a', value: 1 }, { key: 'e', value: {} }],
-            untouched: [{ key: 'c', value: 'three' }],
-            updated: [{ key: 'b', from: true, to: false }],
-          },
-        }],
-      },
-    }],
-  });
+  const before = {
+    key: {
+      a: 1,
+      b: true,
+      c: 'three',
+      e: {},
+    },
+  };
+  const after = {
+    key: {
+      b: false,
+      c: 'three',
+      d: 'new',
+      f: {},
+    },
+  };
+  expect(genDiff(before, after)).toEqual([
+    {
+      name: 'key',
+      childs: [
+        {
+          name: 'a',
+          from: 1,
+          to: undefined,
+        }, {
+          name: 'b',
+          from: true,
+          to: false,
+        }, {
+          name: 'c',
+          from: 'three',
+          to: 'three',
+        }, {
+          name: 'd',
+          from: undefined,
+          to: 'new',
+        }, {
+          name: 'e',
+          from: {},
+          to: undefined,
+        }, {
+          name: 'f',
+          from: undefined,
+          to: {},
+        },
+      ],
+    },
+  ]);
 });
--- src/diffGenerator.js
+++ src/diffGenerator.js
@@ -1,32 +1,26 @@
 import _ from 'lodash';
 
-const {
-  union,
-  difference,
-  keys,
-} = _;
 
 const genDiff = (firstObject, secondObject) => {
-  const firstObjectKeys = keys(firstObject);
-  const secondObjectKeys = keys(secondObject);
-
-  const addedKeys = secondObjectKeys.filter((skey) => !firstObjectKeys.includes(skey));
-  const updatedKeys = secondObjectKeys.filter((skey) => firstObject[skey] !== undefined && firstObject[skey] !== secondObject[skey]);
-  const removedKeys = firstObjectKeys.filter((fkey) => !secondObjectKeys.includes(fkey));
-  const untouchedKeys = difference(union(firstObjectKeys, secondObjectKeys), union(addedKeys, updatedKeys, removedKeys));
-
-  const added = addedKeys.map((key) => ({ key, value: secondObject[key] }));
-  const removed = removedKeys.map((key) => ({ key, value: firstObject[key] }));
-  const untouched = untouchedKeys.map((key) => ({ key, value: firstObject[key] }));
-  const updated = updatedKeys.map((key) => (typeof firstObject[key] !== 'object' ? ({ key, from: firstObject[key], to: secondObject[key] }) : ({ key, value: genDiff(firstObject[key], secondObject[key]) })));
-  // const updated = updatedKeys.map((key) => (typeof firstObject[key] !== 'object' ? ({ key, from: firstObject[key], to: secondObject[key] }) : ({ key, value: null })));
-
-  return {
-    added,
-    removed,
-    untouched,
-    updated,
-  };
+  const keys = _.union(_.keys(firstObject), _.keys(secondObject)).sort();
+  const result = [];
+  for (const key of keys) {
+    const from = firstObject[key];
+    const to = secondObject[key];
+    if (typeof from === 'object' && typeof to === 'object') {
+      result.push({
+        name: key,
+        childs: genDiff(firstObject[key], secondObject[key]),
+      });
+    } else {
+      result.push({
+        name: key,
+        from,
+        to,
+      });
+    }
+  }
+  return result;
 };
 
 export default genDiff;
