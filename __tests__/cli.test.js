22edf8883fccff5c032edd3cabfe474c17d4c81a
Alexander Popov <alex.popov.tech@gmail.com>
2020-05-06 01:37:37 +0300

    rename fixtures

--- __tests__/cli.test.js
+++ __tests__/cli.test.js
@@ -6,28 +6,27 @@ import { genDiffString } from '../src/index.js';
     format: 'pretty',
     from: { path: 'fixtures/before.json', type: 'JSON' },
     to: { path: 'fixtures/after.yml', type: 'YAML' },
-    output: readFileSync('fixtures/pretty_output').toString().trim(),
+    expected: readFileSync('fixtures/expected.pretty').toString().trim(),
   },
   {
     format: 'json',
     from: { path: 'fixtures/before.json', type: 'JSON' },
     to: { path: 'fixtures/after.json', type: 'JSON' },
-    output: readFileSync('fixtures/json_output').toString().trim(),
+    expected: readFileSync('fixtures/expected.json').toString().trim(),
   },
   {
     format: 'plain',
     from: { path: 'fixtures/before.yml', type: 'YAML' },
     to: { path: 'fixtures/after.ini', type: 'INI' },
-    output: readFileSync('fixtures/plain_output').toString().trim(),
+    expected: readFileSync('fixtures/expected.plain').toString().trim(),
   },
 ].forEach(({
   format,
   from,
   to,
-  output,
+  expected,
 }) => {
   test(`genDiff provides correct differences in "${format}" between "${from.type}" and "${to.type}"`, () => {
-    expect(genDiffString(from.path, to.path, format))
-      .toEqual(output);
+    expect(genDiffString(from.path, to.path, format)).toEqual(expected);
   });
 });
